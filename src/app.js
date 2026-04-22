9a252b06a3df4fe5699f0bc4e42c52f9577db6dd
Sahan Kaviska <it22233530@my.sliit.lk>
2026-04-22 23:33:44 +0530

    vercel

--- src/app.js
+++ src/app.js
@@ -3,24 +3,52 @@ const helmet = require('helmet');
 const cors = require('cors');
 const rateLimit = require('express-rate-limit');
 const session = require('express-session');
+const swaggerUi = require('swagger-ui-express');
 require('dotenv').config();
 const { passport } = require('./config/passport');
+const swaggerSpec = require('../swagger');
 
 const app = express();
+const defaultAllowedOrigins = [
+  'http://localhost:3001',
+  'http://127.0.0.1:3001',
+  'http://localhost:5173',
+  'http://127.0.0.1:5173',
+];
+const configuredAllowedOrigins = (process.env.ALLOWED_ORIGIN || '')
+  .split(',')
+  .map((origin) => origin.trim())
+  .filter(Boolean);
+const allowedOrigins = new Set([...defaultAllowedOrigins, ...configuredAllowedOrigins]);
+
+const isAllowedOrigin = (origin) => {
+  if (!origin) {
+    return true;
+  }
+
+  try {
+    const { hostname } = new URL(origin);
+    return allowedOrigins.has(origin) || /\.vercel\.app$/.test(hostname);
+  } catch {
+    return false;
+  }
+};
+
+app.get('/api-docs.json', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(swaggerSpec);
+});
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use(helmet());
 app.use(express.json());
 app.use(
   cors({
     origin: function (origin, callback) {
-      if (
-        !origin ||
-        origin === "http://localhost:5173" ||
-        /\.vercel\.app$/.test(new URL(origin).hostname)
-      ) {
+      if (isAllowedOrigin(origin)) {
         return callback(null, true);
       }
-      return callback(new Error("Not allowed by CORS"));
+      return callback(null, false);
     },
     credentials: true,
   })
