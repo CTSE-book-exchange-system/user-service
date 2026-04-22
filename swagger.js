fd5ab971114370782e27bd3a4b2f473e44fd658f
Sahan Kaviska <it22233530@my.sliit.lk>
2026-04-23 01:04:39 +0530

    updated new

--- swagger.js
+++ swagger.js
@@ -1,6 +1,11 @@
 const swaggerJsdoc = require('swagger-jsdoc');
 
-const serverUrl = process.env.API_BASE_URL || '/';
+const PORT = process.env.PORT || 3001;
+const serverUrl =
+  process.env.PUBLIC_API_BASE_URL ||
+  (process.env.RENDER_EXTERNAL_HOSTNAME
+    ? `https://${process.env.RENDER_EXTERNAL_HOSTNAME}`
+    : process.env.API_BASE_URL || `http://localhost:${PORT}`);
 
 module.exports = swaggerJsdoc({
   definition: {
